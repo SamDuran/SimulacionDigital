2ca6684cbca8a525e140f45671c7cd164b6ee0f5
SamDuran <94940561+SamDuran@users.noreply.github.com>
2023-06-11 23:28:41 -0400

    Creating Utils.cs Class
    Adding String to Int custom method

---
+++ SDT_Tarea3/Utils/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDT_Tarea3.Utils
+{
+    static class Utils
+    {
+        static public int ToPossitiveInt(this string input)
+        {
+            if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+                return -1;
+
+            var resultado = "";
+            foreach (var caracter in input)
+            {
+                if (char.IsNumber(caracter))
+                {
+                    resultado += caracter;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            return Convert.ToInt32(resultado);
+        }
+    }
+}
